b106904686a2f9b224a77cf7ae9d2c5521674d2d
MistaFish31 <zhang.ma@husky.neu.edu>
2016-02-16 18:30:09 -0500

    problem 12

--- extraProblems.js
+++ extraProblems.js
@@ -1,4 +1,4 @@
-/*ELEVEN*/
+/*ELEVEN(UNFINISHED)*/
 
 var outputName=function(name){
   result "Hello"+name
@@ -6,7 +6,18 @@ var outputName=function(name){
 
 console.log(outputName("Maggie"))
 
-A function that takes in a String (name) and outputs
-"Hello ${name}" where "#{name}" is the inputted name.
+/*TWELVE*/
 
+var anArray=[14,3,5,"Bob","Alfred", 3,5,7];
 
+var everyOther=function(anArray){
+  var result=""
+  for(var i=1; i<anArray.length; i+=2){
+    result+=anArray[i]
+  }
+  return result
+}
+
+console.log(everyOther(anArray))
+
+/*keep the scope to "anArray", to keep in general. Don't use sampleArray!*/
