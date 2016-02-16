9d48ae668ea6d73366aa47eca03343ed56166878
MistaFish31 <zhang.ma@husky.neu.edu>
2016-02-16 16:03:38 -0500

    Hello "name"

---
+++ extraProblems.js
@@ -0,0 +1,12 @@
+/*ELEVEN*/
+
+var outputName=function(name){
+  result "Hello"+name
+}
+
+console.log(outputName("Maggie"))
+
+A function that takes in a String (name) and outputs
+"Hello ${name}" where "#{name}" is the inputted name.
+
+
